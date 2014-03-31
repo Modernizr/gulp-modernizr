7314feaebdaaf979ee39d0b6fc4cb0e2416cf604
Richard Herrera <rich@doctyper.com>
2014-03-31 10:52:42 -0700

    Make sure to return callback at every turn.

--- index.js
+++ index.js
@@ -43,15 +43,18 @@ module.exports = function (fileName, opt) {
 
 		// Return if null
 		if (file.isNull()) {
-			return;
+			stream.push(file);
+			return callback();
 		}
 
 		// No stream support (yet?)
 		if (file.isStream()) {
-			return stream.emit("error", new gutil.PluginError({
+			stream.emit("error", new gutil.PluginError({
 				plugin: PLUGIN_NAME,
 				message: "Streaming not supported"
 			}));
+
+			return callback();
 		}
 
 		// Set first file
@@ -62,13 +65,12 @@ module.exports = function (fileName, opt) {
 		// Save buffer for later use
 		opt.files.src.push(file);
 
-		callback();
+		return callback();
 	}
 
-	function generateModernizr() {
-
+	function generateModernizr(callback) {
 		if (opt.files.src.length === 0) {
-			return;
+			return callback();
 		}
 
 		// Call customizr
@@ -92,6 +94,8 @@ module.exports = function (fileName, opt) {
 
 			// Pass data along
 			stream.push(file);
+
+			return callback();
 		});
 	}
 
