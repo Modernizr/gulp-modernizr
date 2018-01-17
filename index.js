2ef6fe0c6dc2cc31164349069d7ed6a13c77d5f6
Veeck <github@veeck.de>
2018-01-17 21:31:35 +0100

    Use ' instead of "

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var path = require("path"),
+var path = require('path'),
 	PluginError = require('plugin-error'),
 	Vinyl = require('vinyl'),
-	through = require("through2"),
-	customizr = require("customizr");
+	through = require('through2'),
+	customizr = require('customizr');
 
 module.exports = function (fileName, opt) {
-	"use strict";
+	'use strict';
 
 	// Set some defaults
-	var PLUGIN_NAME = "gulp-modernizr",
-		DEFAULT_FILE_NAME = "modernizr.js";
+	var PLUGIN_NAME = 'gulp-modernizr',
+		DEFAULT_FILE_NAME = 'modernizr.js';
 
 	// Ensure fileName exists
-	if (typeof fileName === "undefined") {
+	if (typeof fileName === 'undefined') {
 		fileName = opt ? opt.dest : DEFAULT_FILE_NAME;
 	} else if (typeof fileName === typeof {}) {
 		opt = fileName;
@@ -50,9 +50,9 @@ module.exports = function (fileName, opt) {
 
 		// No stream support (yet?)
 		if (file.isStream()) {
-			stream.emit("error", new PluginError({
+			stream.emit('error', new PluginError({
 				plugin: PLUGIN_NAME,
-				message: "Streaming not supported"
+				message: 'Streaming not supported'
 			}));
 
 			return callback();
@@ -79,9 +79,9 @@ module.exports = function (fileName, opt) {
 
 			// Sanity check
 			if (!data.result) {
-				return stream.emit("error", new PluginError({
+				return stream.emit('error', new PluginError({
 					plugin: PLUGIN_NAME,
-					message: "No data returned"
+					message: 'No data returned'
 				}));
 			}
 
