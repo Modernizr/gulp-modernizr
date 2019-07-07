40e04180e86dd69ae8e0b88f43a7a26e85a01616
rejas <github@veeck.de>
2019-07-07 10:20:37 +0200

    Split up tests

--- test/tests.js
+++ test/tests.js
@@ -16,16 +16,37 @@ describe('gulp-modernizr', function() {
         .pipe(modernizr(
           'modernizr.js', {
             quiet: true,
-            tests: ['history'],
-            excludeTests: ['dart'],
+          }));
+
+      stream.on('data', function(file) {
+        assert.notEqual(-1, String(file.path).indexOf('modernizr.js'));
+        assert.notEqual(-1, String(file.contents).indexOf('Modernizr'));
+        done();
+      });
+
+      stream.write(new Vinyl({
+        path: TEST_PATH,
+        contents: fs.readFileSync(TEST_PATH),
+      }));
+
+      stream.end();
+    });
+
+    it('should add options to the generated Modernizr file', function(done) {
+      const TEST_PATH = __dirname + '/vanilla.js';
+
+      const stream = gulp.src(TEST_PATH)
+        .pipe(modernizr(
+          'modernizr.js', {
+            quiet: true,
             options: ['setClasses', 'html5printshiv'],
           }));
 
       stream.on('data', function(file) {
         assert.notEqual(-1, String(file.path).indexOf('modernizr.js'));
-        assert.notEqual(-1, String(file.contents.toString()).indexOf('Modernizr'));
-        assert.equal(-1, String(file.contents.toString()).indexOf('dart'));
-        assert.notEqual(-1, String(file.contents.toString()).indexOf('html5printshiv'));
+        assert.notEqual(-1, String(file.contents).indexOf('Modernizr'));
+        assert.notEqual(-1, String(file.contents).indexOf('html5printshiv'));
+        assert.notEqual(-1, String(file.contents).indexOf('setClasses'));
         done();
       });
 
@@ -37,21 +58,17 @@ describe('gulp-modernizr', function() {
       stream.end();
     });
 
-    it('should generate a custom Modernizr file with custom tests', function(done) {
+    it('should add custom tests to the generated Modernizr file', function(done) {
+      const TEST_PATH = __dirname + '/vanilla.js';
 
-      var stream = modernizr({
-        crawl: false,
-        tests: [
-          'touchevents',
-        ],
-      });
-
-      var TEST_PATH = __dirname + '/vanilla.js';
+      const stream = gulp.src(TEST_PATH)
+        .pipe(modernizr({
+          quiet: true,
+          tests: ['history'],
+        }));
 
       stream.on('data', function(file) {
-        assert.notEqual(-1, String(file.path).indexOf('modernizr.js'));
-        assert.equal(-1, String(file.contents).indexOf('webworkers'));
-
+        assert.notEqual(-1, String(file.contents).indexOf('history'));
         done();
       });
 
@@ -61,11 +78,34 @@ describe('gulp-modernizr', function() {
       }));
 
       stream.end();
-
     });
 
-    it('should generate a custom Modernizr file with custom tests and no source files', function(done) {
-      const stream = gs('fake', { allowEmpty: true })
+    it('should not add excluded tests to the generated Modernizr file', function(done) {
+      const TEST_PATH = __dirname + '/vanilla.js';
+
+      const stream = gulp.src(TEST_PATH)
+        .pipe(modernizr({
+          quiet: true,
+          excludeTests: ['dart'],
+        }));
+
+      stream.on('data', function(file) {
+        assert.equal(-1, String(file.contents).indexOf('dart'));
+        done();
+      });
+
+      stream.write(new Vinyl({
+        path: TEST_PATH,
+        contents: fs.readFileSync(TEST_PATH),
+      }));
+
+      stream.end();
+    });
+
+    it('should respect the crawl option set to false', function(done) {
+      const FAKE_TEST_PATH = 'fake';
+
+      const stream = gulp.src(FAKE_TEST_PATH, { allowEmpty: true })
         .pipe(modernizr({
           crawl: false,
           tests: [
